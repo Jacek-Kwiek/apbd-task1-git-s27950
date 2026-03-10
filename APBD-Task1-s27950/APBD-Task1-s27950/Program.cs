b9033c2b91ec54fe7d71f95a04bb0e3b052138f9
Jacek Kwiek <s27950@pjwstk.edu.pl>
2026-03-10 13:32:32 +0100

    meaningful commit 2 - created list of numbers

--- APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
+++ APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Wolrd 2");
+
+int[] intlist = new int[10] {1,2,3,4,5,6,7,8,9,10};
