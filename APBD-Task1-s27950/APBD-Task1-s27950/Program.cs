209728dcb7cdd8f4f72fd983e760d26210aecdfe
Jacek Kwiek <s27950@pjwstk.edu.pl>
2026-03-10 13:33:57 +0100

    meaningful commit 3 - created for loop to add numbers

--- APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
+++ APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Wolrd 2");
 
 int[] intlist = new int[10] {1,2,3,4,5,6,7,8,9,10};
+
+int sum = 0;
+
+for (int i = 0; i < intlist.Length; i++)
+{
+    sum += intlist[i];
+}
