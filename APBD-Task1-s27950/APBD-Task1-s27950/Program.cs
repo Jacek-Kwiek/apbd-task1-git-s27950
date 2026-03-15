81d5933bec799281dfb076d125935881e2aea33b
Jacek Kwiek <s27950@pjwstk.edu.pl>
2026-03-15 22:35:43 +0100

    added comment to main branch

--- APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
+++ APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
@@ -4,6 +4,8 @@ using System.Diagnostics.CodeAnalysis;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Wolrd 2");
 
+// Interesting comment
+
 int[] intlist = new int[10] {1,2,3,4,5,6,7,8,9,10};
 
 int sum = 0;
