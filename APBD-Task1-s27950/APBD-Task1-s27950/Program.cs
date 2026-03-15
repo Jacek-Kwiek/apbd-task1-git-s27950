7941c379f488e3c129d800a50577c2cca95e53c7
Jacek Kwiek <s27950@pjwstk.edu.pl>
2026-03-15 22:33:58 +0100

    Created feature min and created function min() which returns lowest value integer in integer array

--- APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
+++ APBD-Task1-s27950/APBD-Task1-s27950/Program.cs
@@ -20,6 +20,8 @@ Console.WriteLine("average of numbers: " + sum/intlist.Length);
 Console.WriteLine("average of numbers with function:" + average(intlist));
 
 Console.WriteLine("maximum value in list: " + max(intlist));
+
+Console.WriteLine("minimum value in list: " + min(intlist));
 static int average(int[] intlist)
 {
 
@@ -41,4 +43,14 @@ static int max(int[] intlist)
         if(intlist[i] > max) { max = intlist[i]; }
     }
     return max;
+}
+
+static int min(int[] intlist)
+{
+    int min = intlist[0];
+    for (int i = 1; i < intlist.Length; i++)
+    {
+        if (intlist[i] < min) { min = intlist[i]; }
+    }
+    return min;
 }
